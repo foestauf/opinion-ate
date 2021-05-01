b004586416d1f1c72af854daaaefb64c58cda9c1
foestauf <46908845+foestauf@users.noreply.github.com>
2021-05-01 16:23:23 -0400

    Error message

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,14 +2,23 @@ import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { loadRestaurants } from '../store/restaurants/actions';
 import { CircularProgress, List, ListItem } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 
-export const RestaurantList = ({ loadRestaurants, restaurants, loading }) => {
+export const RestaurantList = ({
+  loadRestaurants,
+  restaurants,
+  loading,
+  loadError,
+}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
   return (
     <>
       {loading && <CircularProgress data-testid="loading-indicator" />}
+      {loadError && (
+        <Alert severity="error">Restaurants could not be loaded.</Alert>
+      )}
       <List>
         {restaurants.map(restaurant => (
           <ListItem key={restaurant.id}>{restaurant.name}</ListItem>
