a8b18b7104e2de726427a1e8744b0efb8b17fd53
foestauf <46908845+foestauf@users.noreply.github.com>
2021-05-01 08:17:25 -0400

    Apply formatting

--- src/components/RestaurantScreen.jsx
+++ src/components/RestaurantScreen.jsx
@@ -1,12 +1,12 @@
-import RestaurantList from "./RestaurantList";
+import RestaurantList from './RestaurantList';
 
 export const RestaurantScreen = () => {
-    return (
+  return (
     <div>
-        <h1>Restaurants</h1>
-        <RestaurantList />
+      <h1>Restaurants</h1>
+      <RestaurantList />
     </div>
-    )
-}
+  );
+};
 
 export default RestaurantScreen;
