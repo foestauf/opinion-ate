76a44ebeb40c5cef6d6846b3d4f182f15898bfc9
foestauf <46908845+foestauf@users.noreply.github.com>
2021-05-01 16:11:12 -0400

    Adding loading indicator

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,22 +1,27 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { loadRestaurants } from '../store/restaurants/actions';
+import { CircularProgress, List, ListItem } from '@material-ui/core';
 
-export const RestaurantList = ({ loadRestaurants, restaurants }) => {
+export const RestaurantList = ({ loadRestaurants, restaurants, loading }) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
   return (
-    <ul>
-      {restaurants.map(restaurant => (
-        <li key={restaurant.id}>{restaurant.name}</li>
-      ))}
-    </ul>
+    <>
+      {loading && <CircularProgress data-testid="loading-indicator" />}
+      <List>
+        {restaurants.map(restaurant => (
+          <ListItem key={restaurant.id}>{restaurant.name}</ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
 const mapStateToProps = state => ({
   restaurants: state.restaurants.records,
+  loading: state.restaurants.loading,
 });
 
 const mapDispatchToProps = { loadRestaurants };
--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -4,13 +4,26 @@ import restaurantReducer from '../restaurants/reducers';
 import { loadRestaurants } from '../restaurants/actions';
 
 describe('restaurants', () => {
-  describe('loadRestaurants action', () => {
-    it('store the restaurants', async () => {
-      const records = [
-        { id: 1, name: 'Sushi Place' },
-        { id: 2, name: 'Pizza Place' },
-      ];
+  describe('initially', () => {
+    it('does not have the loading flag set', () => {
+      const initialState = {};
 
+      const store = createStore(
+        restaurantReducer,
+        initialState,
+        applyMiddleware(thunk),
+      );
+      expect(store.getState().loading).toEqual(false);
+    });
+  });
+  describe('loadRestaurants action', () => {});
+  describe('when loading succeeds', () => {
+    const records = [
+      { id: 1, name: 'Sushi Place' },
+      { id: 2, name: 'Pizza Place' },
+    ];
+    let store;
+    beforeEach(() => {
       const api = {
         loadRestaurants: () => Promise.resolve(records),
       };
@@ -19,15 +32,34 @@ describe('restaurants', () => {
         records: [],
       };
 
+      store = createStore(
+        restaurantReducer,
+        initialState,
+        applyMiddleware(thunk.withExtraArgument(api)),
+      );
+      return store.dispatch(loadRestaurants());
+    });
+    it('store the restaurants', () => {
+      expect(store.getState().records).toEqual(records);
+    });
+    it('clears teh loading flag', () => {
+      expect(store.getState().loading).toEqual(false);
+    });
+  });
+  describe('while loading', () => {
+    it('sets a loading flag', () => {
+      const api = {
+        loadRestaurants: () => new Promise(() => {}),
+      };
+      const initialState = {};
+
       const store = createStore(
         restaurantReducer,
         initialState,
         applyMiddleware(thunk.withExtraArgument(api)),
       );
-
-      await store.dispatch(loadRestaurants());
-
-      expect(store.getState().records).toEqual(records);
+      store.dispatch(loadRestaurants());
+      expect(store.getState().loading).toEqual(true);
     });
   });
 });
