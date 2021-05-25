a1dc3d91b09cec8467e34a1acdc20917494caaa5
foestauf <46908845+foestauf@users.noreply.github.com>
2021-05-24 22:03:05 -0400

    Add alert for empty name field

--- src/components/__tests__/NewRestaurantForm.spec.js
+++ src/components/__tests__/NewRestaurantForm.spec.js
@@ -5,12 +5,19 @@ import { NewRestaurantForm } from '../NewRestaurantForm';
 
 describe('NewRestaurantForm', () => {
   const restaurantName = 'Sushi Place';
+  const requiredError = 'Name is required';
   let createRestaurant;
   let context;
 
   beforeEach(() => {
     createRestaurant = jest.fn().mockName('createRestaurant');
     context = render(<NewRestaurantForm createRestaurant={createRestaurant} />);
+  });
+  describe('initially', () => {
+    it('does not display a validation error', () => {
+      const { queryByText } = context;
+      expect(queryByText(requiredError)).toBeNull();
+    });
   });
   describe('when filled in', () => {
     beforeEach(async () => {
@@ -31,6 +38,25 @@ describe('NewRestaurantForm', () => {
     it('clears the name', () => {
       const { getByPlaceholderText } = context;
       expect(getByPlaceholderText('Add Restaurant').value).toEqual('');
+    });
+    it('does not display a validation error', () => {
+      const { queryByText } = context;
+      expect(queryByText(requiredError)).toBeNull();
+    });
+  });
+  describe('when empty', () => {
+    beforeEach(async () => {
+      createRestaurant.mockResolvedValue();
+
+      const { getByPlaceholderText, getByTestId } = context;
+      await userEvent.type(getByPlaceholderText('Add Restaurant'), '');
+      userEvent.click(getByTestId('new-restaurant-submit-button'));
+
+      return act(flushPromises);
+    });
+    it('displays a validation error', () => {
+      const { queryByText } = context;
+      expect(queryByText(requiredError)).not.toBeNull();
     });
   });
 });
